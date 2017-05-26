2999b26212519f46f1d50321edefc42ad00195a4
swapneel <swapiitr@gmail.com>
2017-05-26 14:31:13 +0530

    modified walk2.m

--- walk2.m
+++ walk2.m
@@ -1,10 +1,14 @@
-function out = walk2(x,y,img)
+function [out] = walk2(x,y,img)
+set(0,'RecursionLimit',1000)
 count1=0;
+error1='hit the wall';
+img(x,y)=img(x,y)+1;
 for p = (x-1):(x+1)
 	stp=0;
 	for q = (y-1):(y+1)
 		if img(p,q)==150
-			out=[p,q]
+			out=[p,q];
+			img(p,q)=img(p,q)+1;
 			stp=1;
 		elseif img(p,q)==255
 			count1=count1+1;
@@ -14,11 +18,11 @@ for p = (x-1):(x+1)
 		break
 	end
 end
-if count1<=2
+if count1<2
 	for a = (x-1):(x+1)
 		for b = (y-1):(y+1)
-			if a~=x || b~=y
-				walk(a,b,img)
+			if img(a,b)==0
+				walk2(a,b,img)
 			end
 		end
 	end
