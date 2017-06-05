a4c7c9ad08a7cb1d4509c91d033e3862522b9ee9
swapneel <swapiitr@gmail.com>
2017-06-05 14:36:47 +0530

    changes made in these files

--- vertex.m
+++ vertex.m
@@ -1,11 +1,59 @@
 function out = vertex(x,y,image)
 global stack1
+global xmin
+global xmax
+inter=[];
 elephantwalk(x,y,image)
-a = stack1;
-xmin = min(stack1(:,1));
-xmax = max(stack1(:,1));
-[tj index] = ismember(xmin,stack1);
-b = [xmin stack1(index,2)]
-
+unsorted_mat= stack1
+xmin = min(unsorted_mat(:,1));
+xmax = max(unsorted_mat(:,1));
+[~, indexi] = ismember(xmin,unsorted_mat);
+sorted_mat = [xmin unsorted_mat(indexi,2)];
+unsorted_mat(indexi,:) = [];
+xmin = min(unsorted_mat(:,1));
+[~, indexi] = ismember(xmin,unsorted_mat);
+if unsorted_mat(indexi,2) < sorted_mat(1,2)
+	sorted_mat = [sorted_mat ; [xmin unsorted_mat(indexi,2)]];
+	unsorted_mat(indexi,:) = [];
+	for i = 1:length(unsorted_mat(:,1))
+		xmin = min(unsorted_mat(:,1));
+		[~, indexi] = ismember(xmin,unsorted_mat);
+		if unsorted_mat(indexi,2) < sorted_mat(1,2)
+				sorted_mat = [sorted_mat ; [xmin unsorted_mat(indexi,2)]];
+				unsorted_mat(indexi,:) = [];
+		else
+			inter = [inter ; [xmin unsorted_mat(indexi,2)]];
+			unsorted_mat(indexi,:) = [];	
+		end
+	end
+	for j = 1:length(inter(:,1))
+		xmax = max(inter(:,1));
+		[~, indexi] = ismember(xmax,inter);
+		sorted_mat = [sorted_mat ; [xmax inter(indexi,2)]];
+		inter(indexi,:) = [];
+	end
+	out = sorted_mat;
+elseif unsorted_mat(indexi,2) > sorted_mat(1,2)
+	sorted_mat = [sorted_mat ; [xmin unsorted_mat(indexi,2)]];
+	unsorted_mat(indexi,:) = [];
+	for i = 1:length(unsorted_mat(:,1))
+		xmin = min(unsorted_mat(:,1));
+		[~, indexi] = ismember(xmin,unsorted_mat);
+		if unsorted_mat(indexi,2) > sorted_mat(1,2)
+				sorted_mat = [sorted_mat ; [xmin unsorted_mat(indexi,2)]];
+				unsorted_mat(indexi,:) = [];
+		else
+			inter = [inter ; [xmin unsorted_mat(indexi,2)]];
+			unsorted_mat(indexi,:) = [];	
+		end
+	end
+	for j = 1:length(inter(:,1))
+		xmax = max(inter(:,1));
+		[~, indexi] = ismember(xmax,inter);
+		sorted_mat = [sorted_mat ; [xmax inter(indexi,2)]];
+		inter(indexi,:) = [];
+	end
+	out = sorted_mat;
+end
 stack1 = [];
 end
