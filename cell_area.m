459202611c586d87d473b0d77be8a837b264fe27
swapneel <swapiitr@gmail.com>
2017-06-07 11:13:02 +0530

    added peremeter and vertices to the output

--- cell_area.m
+++ cell_area.m
@@ -1,11 +1,15 @@
-function out = cell_area(x,y,image)
-cellar=0;
-vertices = vertex(x,y,image)
+function [carea cperem vertices] = cell_area(x,y,image)
+cellar=0.0;
+cellper=0.0;
+vertices = vertex(x,y,image);
 for i = 1:length(vertices(:,1))
 	if i ~= length(vertices(:,1))
 		cellar = cellar + (vertices(i,1).*vertices(i+1,2)) - (vertices(i+1,1).*vertices(i,2));
+		cellper = cellper + sqrt((vertices(i+1,1)-vertices(i,1)).^2 + (vertices(i+1,2)-vertices(i,2)).^2);
 	else
 		cellar = cellar + (vertices(i,1).*vertices(1,2)) - (vertices(1,1).*vertices(i,2));
+		cellper = cellper + sqrt((vertices(1,1)-vertices(i,1)).^2 + (vertices(1,2)-vertices(i,2)).^2);
 	end
 end
-out = abs(cellar./2);
+carea = abs(cellar./2);
+cperem = cellper;
